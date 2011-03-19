1c47436f37c86c7b570624a2b8b6e0438c2a0fe4
Savanah Cummings <savanah.cummings@gmail.com>
2011-03-18 23:19:11 -0400

    Out of the box Xcode 3.2.5 generated iPhone window-based application

---
+++ Classes/TTURLRequestModelSampleAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TTURLRequestModelSampleAppDelegate.h
+//  TTURLRequestModelSample
+//
+//  Created by Savanah Cummings on 3/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTURLRequestModelSampleAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
